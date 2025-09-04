6059ecef080112bab0dc262918987a55c5586c67
DiamonDoughnut <calebssmith@hotmail.com>
2025-09-04 17:11:32 -0500

    messing up formatting to fail new tests

--- handler_ready_test.go
+++ handler_ready_test.go
@@ -12,12 +12,12 @@ func TestHandlerReadiness(t *testing.T) {
 
 	handlerReadiness(w, req)
 
-	if w.Code != http.StatusOK {
+	if w.Code != http.StatusOK{
 		t.Errorf("expected status %d, got %d", http.StatusOK, w.Code)
 	}
 
 	expected := `{"status":"ok"}`
-	if w.Body.String() != expected {
+	if w.Body.String() != expected{
 		t.Errorf("expected body %s, got %s", expected, w.Body.String())
 	}
 }
